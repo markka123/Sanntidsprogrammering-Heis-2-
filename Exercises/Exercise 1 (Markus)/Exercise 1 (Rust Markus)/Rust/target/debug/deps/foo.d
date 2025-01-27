38f83e8966adfe6aee0731f927442695d11f3a9c
markka123 <markus.gk@outlook.com>
2025-01-27 10:26:54 +0100

    updated docs

---
+++ Exercises/Exercise 1 (Markus)/Exercise 1 (Rust Markus)/Rust/target/debug/deps/foo.d
@@ -0,0 +1,5 @@
+C:\Users\marku\Documents\Vår 25\Sanntid\Sanntidsprogrammering-Heis-2-\Exercises\Exercise 1 (Markus)\Exercise 1 (Rust Markus)\Rust\target\debug\deps\foo.exe: src\main.rs
+
+C:\Users\marku\Documents\Vår 25\Sanntid\Sanntidsprogrammering-Heis-2-\Exercises\Exercise 1 (Markus)\Exercise 1 (Rust Markus)\Rust\target\debug\deps\foo.d: src\main.rs
+
+src\main.rs:
