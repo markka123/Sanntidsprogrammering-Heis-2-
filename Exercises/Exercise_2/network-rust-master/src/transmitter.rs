3ca03b952e70ce8707f80750513d6690ae99c27a
Mattis <Mattis@epost.no>
2025-02-03 13:07:36 +0100

    Fixed transmission with UDP

--- Exercises/Exercise_2/network-rust-master/src/transmitter.rs
+++ Exercises/Exercise_2/network-rust-master/src/transmitter.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 
 use crossbeam_channel as cbc;
 use network_rust::udpnet;
+use std::net::UdpSocket;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 struct CustomDataType {
@@ -20,13 +21,17 @@ fn main() -> std::io::Result<()> {
         format!("Sender#{}", process::id())
     };
 
-    let msg_port = 19745;
+    let msg_port = 20003;
+    let target_ip = "10.100.23.204";
+    let target_address = format!("{}:{}", target_ip, msg_port);
+
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
 
     let (custom_data_send_tx, custom_data_send_rx) = cbc::unbounded::<CustomDataType>();
     // Periodisk sending av data
     std::thread::spawn(move || {
         let mut cd = CustomDataType {
-            message: format!("Hello from {}", id),
+            message: format!("Hallaballa {}", id),
             iteration: 0,
         };
         loop {
@@ -37,7 +42,12 @@ fn main() -> std::io::Result<()> {
     });
 
     // Sender data på nettverket
-    udpnet::bcast::tx(msg_port, custom_data_send_rx);
+    //udpnet::bcast::tx(msg_port, custom_data_send_rx);
+    for data in custom_data_send_rx.iter() {
+        let serialized_data = serde_json::to_string(&data).unwrap();
+        socket.send_to(serialized_data.as_bytes(), &target_address)?;
+        println!("Sent message: {}", serialized_data);
+    }
     
 
     Ok(())
