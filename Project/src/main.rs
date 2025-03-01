680e4ba2c03e523cfca5d587df8f5f48d53b3251
henrikOes <henrik.ostvik@gmail.com>
2025-03-01 11:46:53 +0100

    Extended to run multiple servers locally

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -10,9 +10,28 @@ use driver_rust::network;
 use driver_rust::network::udp;
 use std::sync::Arc;
 use std::thread::*;
+use std::env;
 
 fn main() -> std::io::Result<()> {
-    let elevator = e::Elevator::init("localhost:15657", config::ELEV_NUM_FLOORS)?;
+
+    let args: Vec<String> = env::args().collect();
+
+    let default_port = 15657;
+    let port: u16 = if args.len() > 1 {
+        match args[1].parse() {
+            Ok(p) => p,
+            Err(_) => {
+                eprintln!("Warning: Invalid port provided. Using default: {}", default_port);
+                default_port
+            }
+        }
+    } else {
+        default_port
+    };
+    let addr = format!("localhost:{}", port);
+
+
+    let elevator = e::Elevator::init(&addr, config::ELEV_NUM_FLOORS)?;
     println!("Elevator started:\n{:#?}", elevator);
 
     let (new_order_tx, new_order_rx) = cbc::unbounded::<Orders>();
