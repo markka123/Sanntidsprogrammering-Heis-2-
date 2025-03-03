36a40ada5ffe8adeb978ba728909658624254075
markka123 <markus.gk@outlook.com>
2025-03-03 14:10:02 +0100

    Command line args fix

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -14,67 +14,46 @@ use std::thread::*;
 use std::env;
 
 fn main() -> std::io::Result<()> {
-    // let elevator_variables = vec![vec!["moving".to_string(), "2".to_string(), "up".to_string()]];
-    // let cab_requests = vec![vec![false, false, true, true]];
-    // let hall_requests = vec![vec![false, false], vec![true, false], vec![false, false], vec![false, true]];
-
-
-    // let result = assign_requests(&elevator_variables, &cab_requests, &hall_requests);
-    // println!("Result from executable:\n{}", result);
     
-    println!("{}", 1.to_string());
+    let (port, elev_id) = fetch_command_line_args();
 
-    let args: Vec<String> = env::args().collect();
-
-    let default_port = 15657;
-    let port: u16 = if args.len() > 1 {
-        match args[1].parse() {
-            Ok(p) => p,
-            Err(_) => {
-                eprintln!("Warning: Invalid port provided. Using default: {}", default_port);
-                default_port
-            }
-        }
-    } else {
-        default_port
-    };
-    let addr = format!("localhost:{}", port);
+    println!("{}, {}", port, elev_id);
 
 
-    let elevator = e::Elevator::init(&addr, config::ELEV_NUM_FLOORS)?;
-    println!("Elevator started:\n{:#?}", elevator);
+    // let elevator = e::Elevator::init(&addr, config::ELEV_NUM_FLOORS)?;
+    // println!("Elevator started:\n{:#?}", elevator);
 
-    let (new_order_tx, new_order_rx) = cbc::unbounded::<Orders>();
-    let (emergency_reset_tx, emergency_reset_rx) = cbc::unbounded::<bool>();
-    let (new_state_tx, new_state_rx) = cbc::unbounded::<elevator_controller::elevator_fsm::State>();
-    let (order_completed_tx, order_completed_rx) = cbc::unbounded::<elevio::poll::CallButton>();
+    // let (new_order_tx, new_order_rx) = cbc::unbounded::<Orders>();
+    // let (emergency_reset_tx, emergency_reset_rx) = cbc::unbounded::<bool>();
+    // let (new_state_tx, new_state_rx) = cbc::unbounded::<elevator_controller::elevator_fsm::State>();
+    // let (order_completed_tx, order_completed_rx) = cbc::unbounded::<elevio::poll::CallButton>();
 
-    {
-        let elevator = elevator.clone();
-        spawn(move || {
-            elevator_controller::elevator_fsm::elevator_fsm(
-                &elevator,
-                new_order_rx,
-                order_completed_tx,
-                emergency_reset_tx,
-                &new_state_tx,
-            )
-        });
-    }
+    // {
+    //     let elevator = elevator.clone();
+    //     spawn(move || {
+    //         elevator_controller::elevator_fsm::elevator_fsm(
+    //             &elevator,
+    //             new_order_rx,
+    //             order_completed_tx,
+    //             emergency_reset_tx,
+    //             &new_state_tx,
+    //         )
+    //     });
+    // }
 
-    {
-        let elevator = elevator.clone();
-        spawn(move || {
-            distributor::distributor::distributor(
-                &elevator,
-                new_state_rx,
-                order_completed_rx,
-                new_order_tx,
-            )
-        });
-    }
+    // {
+    //     let elevator = elevator.clone();
+    //     spawn(move || {
+    //         distributor::distributor::distributor(
+    //             &elevator,
+    //             new_state_rx,
+    //             order_completed_rx,
+    //             new_order_tx,
+    //         )
+    //     });
+    // }
 
-    let mut all_orders = AllOrders::init();
+    // let mut all_orders = AllOrders::init();
 
     loop {
         // cbc::select! {
@@ -93,4 +72,40 @@ fn main() -> std::io::Result<()> {
         // }
     }
     Ok(())
+}
+
+
+
+pub fn fetch_command_line_args() -> (u16, u8) {
+
+    let default_port = 15657;
+    let default_elev_id = 0;
+
+    let command_line_args: Vec<String> = env::args().collect();
+
+    let port = if command_line_args.len() > 1 {
+        match command_line_args[1].parse::<u16>() {
+            Ok(p) => p,
+            Err(_) => {
+                eprintln!("Warning: Invalid port provided. Using default: {}", default_port);
+                default_port
+            }
+        }
+    } else {
+        default_port
+    };
+
+    let elevator_id = if command_line_args.len() > 2 {
+        match command_line_args[2].parse::<u8>() {
+            Ok(id) => id,
+            Err(_) => {
+                eprintln!("Warning: Invalid elevator ID provided. Using default: {}", default_elev_id);
+                default_elev_id
+            }
+        }
+    } else {
+        default_elev_id
+    };
+
+    (port, elevator_id)
 }
