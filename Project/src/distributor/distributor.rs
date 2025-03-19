37193905f759905b3261adb454e6f8faa6056073
Arne Skogan <arne.skogan@online.no>
2025-03-19 11:06:45 +0100

    lagt til distributorOrders

--- Project/src/distributor/distributor.rs
+++ Project/src/distributor/distributor.rs
@@ -41,13 +41,9 @@ pub fn distributor(
 ) {
     //TODO: REVIEW IF INITS CAN BE CLEANED UP: (Init function?)
 
-    let mut all_orders = orders::AllOrders::init();
-    let mut offline_orders: orders::Orders = [[false; 3]; config::ELEV_NUM_FLOORS as usize];
-    let mut unconfirmed_orders: Vec<(u8, poll::CallButton)> = Vec::new();
-    let mut assigned_orders = [[false; 3]; config::ELEV_NUM_FLOORS as usize];
+    let mut distributor_orders = orders::DistributorOrders::init(); 
 
     let mut master_id = config::ELEV_NUM_ELEVATORS;
-    let mut is_online = true;
     let mut last_received_heartbeat = [Instant::now(); config::ELEV_NUM_ELEVATORS as usize];
     
     let unconfirmed_orders_ticker = cbc::tick(config::UNCONFIRMED_ORDERS_TRANSMIT_PERIOD);
@@ -108,12 +104,12 @@ pub fn distributor(
                 let call_button = call_button.unwrap();
                 let msg_type = NEW_ORDER;
 
-                unconfirmed_orders.push((msg_type, call_button.clone()));
+                distributor_orders.unconfirmed_orders.push((msg_type, call_button.clone()));
                 
                 if states[elevator_id as usize].offline {
-                    offline_orders[call_button.floor as usize][call_button.call as usize] = true;
-                    all_orders.add_order(call_button.clone(), elevator_id);
-                    new_order_tx.send((offline_orders, all_orders.hall_orders)).unwrap();
+                    distributor_orders.elevator_orders[call_button.floor as usize][call_button.call as usize] = true;
+                    distributor_orders.add_order(call_button.clone(), elevator_id);
+                    new_order_tx.send((distributor_orders.elevator_orders, distributor_orders.hall_orders)).unwrap();
                 }
                 call_msg_tx.send((msg_type, call_button)).unwrap();
             },
@@ -122,15 +118,15 @@ pub fn distributor(
                 let msg_type = COMPLETED_ORDER;
                 
                 if states[elevator_id as usize].offline {
-                    offline_orders[order_completed.floor as usize][order_completed.call as usize] = false;
-                    all_orders.remove_order(order_completed.clone(), elevator_id);
-                    unconfirmed_orders.retain(|(msg, order)| *msg != msg_type || order.floor != order_completed.floor || order.call != order_completed.call);
-                    new_order_tx.send((offline_orders, all_orders.hall_orders)).unwrap(); 
+                    distributor_orders.elevator_orders[order_completed.floor as usize][order_completed.call as usize] = false;
+                    distributor_orders.remove_order(order_completed.clone(), elevator_id);
+                    distributor_orders.unconfirmed_orders.retain(|(msg, order)| *msg != msg_type || order.floor != order_completed.floor || order.call != order_completed.call);
+                    new_order_tx.send((distributor_orders.elevator_orders, distributor_orders.hall_orders)).unwrap(); 
                 }
                 call_msg_tx.send((msg_type, order_completed)).unwrap();
             },
             recv(unconfirmed_orders_ticker) -> _ => {
-                unconfirmed_orders.iter().for_each(|(msg_type, call)| {
+                distributor_orders.unconfirmed_orders.iter().for_each(|(msg_type, call)| {
                     call_msg_tx.send((*msg_type, call.clone())).unwrap();
                 });
             }
@@ -145,10 +141,10 @@ pub fn distributor(
                         };
                         match msg_type {
                             NEW_ORDER => {
-                                all_orders.add_order(new_order, id);
+                                distributor_orders.add_order(new_order, id);
                             },
                             COMPLETED_ORDER => {
-                                all_orders.remove_order(new_order, id);
+                                distributor_orders.remove_order(new_order, id);
                             },
                             _ => {
                                 //Handle error
@@ -167,23 +163,22 @@ pub fn distributor(
                         last_received_heartbeat[id as usize] = Instant::now();
                     },
                     Ok(Message::AllAssignedOrdersMsg((master_id, all_assigned_orders_str))) => {
-
-                        let all_assigned_orders_map: HashMap<u8, orders::Orders> = serde_json::from_value(all_assigned_orders_str).unwrap();
-
-                        let hall_orders = get_all_hall_orders(&all_assigned_orders_map);
+                        let previous_hall_orders = distributor_orders.get_assigned_hall_orders();
+                        distributor_orders.assigned_orders_map = serde_json::from_value(all_assigned_orders_str).unwrap();
+                        let new_hall_orders = distributor_orders.get_assigned_hall_orders();
 
                         let elevator_is_availible = states[elevator_id as usize].motorstop || states[elevator_id as usize].emergency_stop || states[elevator_id as usize].obstructed || states[elevator_id as usize].offline;
 
                         if !elevator_is_availible {
-                            if let Some(new_assigned_orders) = all_assigned_orders_map.get(&elevator_id) {
-                                if (*new_assigned_orders != assigned_orders)  || (hall_orders != all_orders.hall_orders) {
-                                    assigned_orders = *new_assigned_orders;
-                                    all_orders.hall_orders = hall_orders;
-                                    new_order_tx.send((assigned_orders, all_orders.hall_orders)).unwrap();
+                            if let Some(new_elevator_orders) = distributor_orders.assigned_orders_map.get(&elevator_id) {
+                                if (*new_elevator_orders != distributor_orders.elevator_orders)  || (new_hall_orders != previous_hall_orders) {
+                                    distributor_orders.elevator_orders = *new_elevator_orders;
+                                    distributor_orders.hall_orders = new_hall_orders;
+                                    new_order_tx.send((distributor_orders.elevator_orders, distributor_orders.hall_orders)).unwrap();
                                 }
                             } 
                         }
-                        confirm_orders(&mut unconfirmed_orders, &all_assigned_orders_map, elevator_id);                        
+                        distributor_orders.confirm_orders(elevator_id);                        
                     }
                     Err(e) => {
                     }
@@ -198,7 +193,7 @@ pub fn distributor(
                 master_id = elevator_id;
             },
             recv(master_ticker) -> _ => {
-                let assigned_orders_str = cost_function::assign_orders(&states, &all_orders.cab_orders, &all_orders.hall_orders);
+                let assigned_orders_str = cost_function::assign_orders(&states, &distributor_orders.cab_orders, &distributor_orders.hall_orders);
                 master_transmit_tx.send(assigned_orders_str).unwrap();
             },
             recv(check_heartbeat_ticker) -> _ => {
@@ -210,78 +205,13 @@ pub fn distributor(
                             states[id].offline = true; // change to one "disconnected/unavailable" state?
                         }
                         if id == (elevator_id as usize) {
-                            is_online_tx.send(false).unwrap();
+                            distributor_orders.init_offline_operation(elevator_id);
+                            new_order_tx.send((distributor_orders.elevator_orders, distributor_orders.hall_orders)).unwrap();
                             master_ticker = cbc::never();
                         }
                     }
                 }
-            },
-            // fix, put the below recv case in a functions
-            recv(is_online_rx) -> is_online_msg => {
-                let network_status = is_online_msg.unwrap();
-                if network_status && !is_online {
-                    offline_orders = [[false; 3]; config::ELEV_NUM_FLOORS as usize];
-                    is_online = true;
-                } else if !network_status && is_online {
-                    for (order_type, order) in unconfirmed_orders.iter() {
-                        if *order_type == NEW_ORDER {
-                            offline_orders[order.floor as usize][order.call as usize] = true;
-                        } 
-                    }
-                    let mut floor = 0;
-                    for order in all_orders.cab_orders[elevator_id as usize].iter() {
-                        offline_orders[floor as usize][e::CAB as usize] = *order;
-                        floor += 1;  
-                    }
-                    is_online = false;
-                    all_orders.hall_orders = [[false; 2]; config::ELEV_NUM_FLOORS as usize];
-                    new_order_tx.send((offline_orders, all_orders.hall_orders)).unwrap();
-                }
             }
         }
     }
-}
-
-fn get_all_hall_orders(map: &HashMap<u8, orders::Orders>) -> orders::HallOrders {
-    let mut all_hall_orders = [[false; 2]; config::ELEV_NUM_FLOORS as usize];
-
-    for orders in map.values() {
-        for (floor, call) in orders.iter().enumerate() {
-            all_hall_orders[floor][0] |= call[0];
-            all_hall_orders[floor][1] |= call[1];
-        }
-    }
-    all_hall_orders
-}
-
-// TODO: Where should this be placed? (orders.rs?)
-
-fn confirm_orders(
-    unconfirmed_orders: &mut Vec<(u8, poll::CallButton)>,
-    all_assigned_orders_map: &HashMap<u8, [[bool; 3]; config::ELEV_NUM_FLOORS as usize]>, 
-    elevator_id: u8,
-) {
-    unconfirmed_orders.retain(|(order_type, order)| {
-        let order_is_assigned = |floor: usize, call: usize| 
-            all_assigned_orders_map.iter().any(|(_, assigned_orders)| assigned_orders[floor][call]);
-
-        let order_is_unassigned = |floor: usize, call: usize| 
-            all_assigned_orders_map.iter().all(|(_, assigned_orders)| !assigned_orders[floor][call]);
-
-        match order.call {
-            e::HALL_UP | e::HALL_DOWN => match order_type {
-                &NEW_ORDER => !order_is_assigned(order.floor as usize, order.call as usize),
-                &COMPLETED_ORDER => order_is_unassigned(order.floor as usize, order.call as usize),
-                _ => true,
-            },
-            e::CAB => {
-                if let Some(assigned_orders) = all_assigned_orders_map.get(&elevator_id) {
-                    let cab_is_assigned = assigned_orders[order.floor as usize][order.call as usize];
-                    return !((cab_is_assigned && *order_type == NEW_ORDER) || (!cab_is_assigned && *order_type == COMPLETED_ORDER));
-                }
-                true
-            }
-            _ => true,
-        }
-    });
 }
--- Project/src/elevator_controller/orders.rs
+++ Project/src/elevator_controller/orders.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 use crate::config::config;
-use crate::elevio::elev::{CAB, HALL_DOWN, HALL_UP};
+use crate::elevio::elev;
 use crate::elevio::poll;
 
 use crossbeam_channel as cbc;
@@ -9,14 +9,8 @@ use std::collections::HashMap;
 pub type Orders = [[bool; 3]; config::ELEV_NUM_FLOORS as usize];
 pub type HallOrders = [[bool; 2]; config::ELEV_NUM_FLOORS as usize];
 pub type CabOrders = [[bool; config::ELEV_NUM_FLOORS as usize]; config::ELEV_NUM_ELEVATORS as usize];
-
-#[derive(Clone, Debug, Copy)]
-pub struct AllOrders {
-    // Init with: let matrix = Matrix::new(rows, cols, false);
-    pub hall_orders: HallOrders,
-    pub cab_orders: CabOrders,
-    pub offline_orders: Orders,
-}
+pub const NEW_ORDER: u8 = 0;
+pub const COMPLETED_ORDER: u8 = 1;
 
 #[derive(Clone, Debug, Copy)]
 pub struct ElevatorOrders {
@@ -26,51 +20,101 @@ pub struct ElevatorOrders {
 //functions
 // init, order_at_floor_in_direction, order_in_direction, order_done
 
+#[derive(Clone, Debug)]
 pub struct DistributorOrders {
     pub hall_orders: HallOrders,
     pub cab_orders: CabOrders,
     pub unconfirmed_orders: Vec<(u8, poll::CallButton)>,
-    pub all_assigned_orders: HashMap<u8, Orders>,
+    pub assigned_orders_map: HashMap<u8, Orders>,
     pub elevator_orders: Orders,
 }
 // functions
 // init, add, remove, confirm_orders, get_assigned_hall_orders, init_offline_operation (recv is_online)
-
-impl AllOrders {
+impl DistributorOrders {
     pub fn init() -> Self {
         let hall_orders = [[false; 2]; config::ELEV_NUM_FLOORS as usize];
         let cab_orders = [[false; config::ELEV_NUM_FLOORS as usize]; config::ELEV_NUM_ELEVATORS as usize];
-        let offline_orders = [[false; 3]; config::ELEV_NUM_FLOORS as usize];
+        let unconfirmed_orders = Vec::new();
+        let assigned_orders_map = HashMap::new();
+        let elevator_orders = [[false; 3]; config::ELEV_NUM_FLOORS as usize];
         Self {
             hall_orders,
             cab_orders,
-            offline_orders,
+            unconfirmed_orders,
+            assigned_orders_map,
+            elevator_orders,
         }
     }
     pub fn add_order(&mut self, call_button: poll::CallButton, elevator_id: u8) {
-        if call_button.call == CAB {
+        if call_button.call == elev::CAB {
             self.cab_orders[elevator_id as usize][call_button.floor as usize] = true;
-        } else if call_button.call == HALL_DOWN || call_button.call == HALL_UP {
+        } else if call_button.call == elev::HALL_DOWN || call_button.call == elev::HALL_UP {
             self.hall_orders[call_button.floor as usize][call_button.call as usize] = true;
-        } else {
-            //Handle error
         }
     }
 
     pub fn remove_order(&mut self, call_button: poll::CallButton, elevator_id: u8) {
-        if call_button.call == CAB {
+        if call_button.call == elev::CAB {
             self.cab_orders[elevator_id as usize][call_button.floor as usize] = false;
-        } else if call_button.call == HALL_DOWN || call_button.call == HALL_UP {
+        } else if call_button.call == elev::HALL_DOWN || call_button.call == elev::HALL_UP {
             self.hall_orders[call_button.floor as usize][call_button.call as usize] = false;
-        } else {
-            //Handle error
         }
     }
 
-    pub fn reset_offline_orders(&mut self) {
-        self.offline_orders= [[false; 3]; config::ELEV_NUM_FLOORS as usize];
+    pub fn confirm_orders(&mut self, elevator_id: u8) {
+        let assigned_orders_map = self.assigned_orders_map.clone();
+        self.unconfirmed_orders.retain(|(order_type, order)| {
+            let order_is_assigned = |floor: usize, call: usize| 
+                assigned_orders_map.iter().any(|(_, assigned_orders)| assigned_orders[floor][call]);
+    
+            let order_is_unassigned = |floor: usize, call: usize| 
+                assigned_orders_map.iter().all(|(_, assigned_orders)| !assigned_orders[floor][call]);
+    
+            match order.call {
+                elev::HALL_UP | elev::HALL_DOWN => match order_type {
+                    &NEW_ORDER => !order_is_assigned(order.floor as usize, order.call as usize),
+                    &COMPLETED_ORDER => order_is_unassigned(order.floor as usize, order.call as usize),
+                    _ => true,
+                },
+                elev::CAB => {
+                    if let Some(assigned_orders) = assigned_orders_map.get(&elevator_id) {
+                        let cab_is_assigned = assigned_orders[order.floor as usize][order.call as usize];
+                        return !((cab_is_assigned && *order_type == NEW_ORDER) || (!cab_is_assigned && *order_type == COMPLETED_ORDER));
+                    }
+                    true
+                }
+                _ => true,
+            }
+        });
+    }
+
+    pub fn get_assigned_hall_orders(&mut self) -> HallOrders {
+        let mut all_hall_orders = [[false; 2]; config::ELEV_NUM_FLOORS as usize];
+    
+        for orders in self.assigned_orders_map.values() {
+            for (floor, call) in orders.iter().enumerate() {
+                all_hall_orders[floor][0] |= call[0];
+                all_hall_orders[floor][1] |= call[1];
+            }
+        }
+        all_hall_orders
+    }
+
+    pub fn init_offline_operation(&mut self, id: u8) {
+        for (order_type, order) in self.unconfirmed_orders.iter() {
+            if *order_type == NEW_ORDER {
+                self.elevator_orders[order.floor as usize][order.call as usize] = true;
+            } 
+        }
+        let mut floor = 0;
+        for order in self.cab_orders[id as usize].iter() {
+            self.elevator_orders[floor as usize][elev::CAB as usize] = *order;
+            floor += 1;  
+        }
+        self.hall_orders = [[false; 2]; config::ELEV_NUM_FLOORS as usize];
     }
 }
+
 
 pub fn order_in_direction(orders: &Orders, floor: u8, dir: u8) -> bool {
     match dir {
@@ -99,7 +143,7 @@ pub fn order_in_direction(orders: &Orders, floor: u8, dir: u8) -> bool {
 }
 
 pub fn order_at_floor_in_direction(orders: &Orders, floor: u8, direction: u8) ->  bool {
-    orders[floor as usize][(direction) as usize] || orders[floor as usize][CAB as usize]
+    orders[floor as usize][(direction) as usize] || orders[floor as usize][elev::CAB as usize]
 }
 
 pub fn order_done(
@@ -114,7 +158,7 @@ pub fn order_done(
             call: direction,
         });
     }
-    if orders[floor as usize][CAB as usize] {
-        let _ = order_completed_tx.send(poll::CallButton { floor, call: CAB });
+    if orders[floor as usize][elev::CAB as usize] {
+        let _ = order_completed_tx.send(poll::CallButton { floor, call: elev::CAB });
     }
 }
